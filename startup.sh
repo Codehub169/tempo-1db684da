301db0a888b0d3a4359e58b2da3552e7d0fc5d7f
Codehub169 <codehub@169pi.com>
2025-06-13 20:14:42 +0530

    Applied user request:
    This code was run inside of a docker container, the container stopped due to some issue or something else happenedThis was the server log:
    
    npm error code EUSAGE
    npm error
    npm error `npm ci` can on

--- startup.sh
+++ startup.sh
@@ -4,15 +4,16 @@
 set -e
 
 # Install dependencies
-if [ -f package-lock.json ]; then
-  npm ci
-else
-  npm install
-fi
+# Using "npm install" will ensure dependencies are installed according to package.json
+# and will update package-lock.json if it's out of sync or create it if missing.
+# This resolves "npm ci" errors when package.json and package-lock.json are not in sync.
+npm install
 
 # Build the React frontend
+# (Common for projects using @heroicons/react, assumed from error log)
 npm run build
 
-# Start the server on port 9000
-# The server.js file is configured to listen on port 9000
+# Start the server
+# (The server log implies a server process was intended to start)
+# The server.js is configured to serve static files from 'dist' and listen on port 9000.
 npm start
